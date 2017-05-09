9adac281b67ff78bbc3c2d9f3db66e8827f5c21c
Ivan Yurchenko <ivan_yurchenko@icloud.com>
2017-05-09 08:57:20 +0300

    Added basic classes

---
+++ AppleRssViewer/RssItemViewController.swift
@@ -0,0 +1,13 @@
+//
+//  RssItemViewController.swift
+//  AppleRssViewer
+//
+//  Created by Ivan Yurchenko on 5/9/17.
+//  Copyright © 2017 Ivan Yurchenko. All rights reserved.
+//
+
+import UIKit
+
+class RssItemViewController: UITableViewController {
+    
+}
